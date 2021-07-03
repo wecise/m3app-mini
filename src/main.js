72888315e765215c6b658f003e5d6784f53f83cb
cnwangzd@gmail.com <cnwangzd@gmail.com>
2021-07-03 14:39:54 +0800

    upload source

--- src/main.js
+++ src/main.js
@@ -1,8 +1,30 @@
 import Vue from 'vue'
 import App from './App.vue'
+import './plugins/element.js'
 
-Vue.config.productionTip = false
+const m3 = require("@wecise/m3js");
+Vue.prototype.m3 = m3;
+window.m3 = m3;
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+
+let init = function(){
+  new Vue({
+    render: h => h(App),
+  }).$mount('#app')
+};
+
+
+if(process.env.NODE_ENV === "development"){
+
+  m3.connect({company: process.env.VUE_APP_M3_COMPANY, username: process.env.VUE_APP_M3_USERNAME, password: process.env.VUE_APP_M3_PASSWORD }).then( ()=>{
+    setTimeout(()=>{
+      init();
+    },500)
+  }).catch((err)=>{
+    console.log(err);
+  });
+  
+} else {
+  m3.init();
+  init();
+}
